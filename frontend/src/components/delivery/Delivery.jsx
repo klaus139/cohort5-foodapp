0528e14efae02337fcd38321da882a72123efe1d
Nicholas Igunbor <nicholas.igunbor@decagon.dev>
2024-04-25 20:49:44 +0100

    created the newletter section, the foods section and the delivery section

---
+++ frontend/src/components/delivery/Delivery.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import classes from './delivery.module.css';
+
+const Delivery = () => {
+  return (
+    <div className={classes.delivery}>
+        <div className={classes.titles}>
+            <span className={classes.deliverySubtitle}>Delivery</span>
+            <h2 className={classes.deliverySubtitle}>Always on time for you</h2>
+        </div>
+
+    </div>
+  )
+}
+
+export default Delivery
--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,11 +1,14 @@
 import React, {useState} from 'react'
-import {Link} from 'react-router-dom'
+import {Link, NavLink} from 'react-router-dom'
 import classes from './navbar.module.css'
+
+import "./navbar.css";
 import { AiOutlineShoppingCart, AiOutlineUser} from 'react-icons/ai'
 //import {useDispatch} from 'react-redux'
 
 const Navbar = () => {
     const [isScrolled, setIsScrolled] = useState(false);
+    const [menuOpen, setMenuOpen] = useState(false);
     // const dispatch = useDispatch()
     // const navigate = useNavigate();
 
@@ -22,35 +25,71 @@ const Navbar = () => {
 
     
   return (
-    <div className={`${classes.container} ${isScrolled && classes.scrolled}`}>
-        <div className={classes.wrapper}>
-            <div className={classes.left}>
-                <Link to='/' className={classes.title}>
-                    C5-FoodStore
-                </Link>
+    // <div className={`${classes.container} ${isScrolled && classes.scrolled}`} onClick={()=> setMenuOpen(!menuOpen)}>
+    //     <div className={classes.wrapper}>
+    //         <div className={classes.left}>
+    //             <Link to='/' className={classes.title}>
+    //                 C5-FoodStore
+    //             </Link>
 
-            </div>
-            <div className={classes.center}>
-                <ul className={classes.list}>
-                    <li className={classes.listItem}>
-                        <a href='/'>Home</a>
-                    </li>
-                    <li className={classes.listItem}>
-                        <a href='#contacts'>Contacts</a>
-                    </li>
-                    <li className={classes.listItem}>
-                        <a href='#foods'>Food</a>
-                    </li>
-                    <li className={classes.listItem}>
-                        <a href='#faq'>FAQ</a>
-                    </li>
-                    <li className={classes.listItem}>
-                        <Link to='/create'>Create</Link>
-                    </li>
+    //         </div>
+    //         <div className={classes.center}>
+    //             <ul className={classes.list}>
+    //                 <li className={classes.listItem}>
+    //                     <a href='/'>Home</a>
+    //                 </li>
+    //                 <li className={classes.listItem}>
+    //                     <a href='#contacts'>Contacts</a>
+    //                 </li>
+    //                 <li className={classes.listItem}>
+    //                     <a href='#foods'>Food</a>
+    //                 </li>
+    //                 <li className={classes.listItem}>
+    //                     <a href='#faq'>FAQ</a>
+    //                 </li>
+    //                 <li className={classes.listItem}>
+    //                     <Link to='/create'>Create</Link>
+    //                 </li>
 
-                </ul>
-            </div>
-            <div className={classes.right}>
+    //             </ul>
+    //         </div>
+    //         <div className={classes.right}>
+    //             <AiOutlineUser className={classes.userIcon} />
+    //             <Link to='/cart' className={classes.cartContainer}>
+    //                 <AiOutlineShoppingCart className={classes.cartIcon}/>
+    //                 <div className={classes.cartQuantity}>3</div>
+    //             </Link>
+    //             <button  className={classes.logout}>Logout</button>
+
+
+    //         </div>
+    //     </div>
+        
+    // </div>
+    <nav>
+   <div className={classes.wrapper}>
+   <Link to="/" className={`${classes.left} title`}>
+      C5-FOODAPP
+    </Link>
+    <div className="menu" onClick={() => setMenuOpen(!menuOpen)}>
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+   <div>
+   <ul className={menuOpen ? "open" : ""}>
+      <li>
+        <NavLink to="/about">About</NavLink>
+      </li>
+      <li>
+        <NavLink to="/services">Services</NavLink>
+      </li>
+      <li>
+        <NavLink to="/contact">Contact</NavLink>
+      </li>
+    </ul>
+   </div>
+    <div className={classes.right}>
                 <AiOutlineUser className={classes.userIcon} />
                 <Link to='/cart' className={classes.cartContainer}>
                     <AiOutlineShoppingCart className={classes.cartIcon}/>
@@ -60,9 +99,9 @@ const Navbar = () => {
 
 
             </div>
-        </div>
-        
-    </div>
+    
+   </div>
+  </nav>
   )
 }
 
